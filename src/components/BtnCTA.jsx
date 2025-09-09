712984d32dd5623ffd3e8deb9db7159b8dab3ea9
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-09-09 13:15:22 +0200

    Linking to aboutpage from btn

--- src/components/BtnCTA.jsx
+++ src/components/BtnCTA.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 export default function BtnCTA() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/about");
+  }
+
   return (
-    <button className="btn-cta" formAction={(URL = "/design")}>
+    <button className="btn-cta" onClick={handleClick}>
       Kontakt mig!
     </button>
   );
