38ff173ddc3300c799aebff96420e74af5245e37
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-09-10 15:52:56 +0200

    Justering af BentoGrid

--- src/pages/Design.jsx
+++ src/pages/Design.jsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header";
-import DesignCards from "../components/DesignCards";
 import BentoGrid from "../components/BentoGrid";
 
 export default function Design() {
@@ -8,18 +7,14 @@ export default function Design() {
       <header>
         <Header />
       </header>
-      <main className="flex flex-col justify-center min-h-screen w-screen bg-[url('/img/backgrounds/BaggrundDesign.svg')] bg-center bg-no-repeat">
+      <main className="flex flex-col justify-center min-h-screen w-screen bg-[url('/img/backgrounds/BaggrundDesign.svg')] bg-fixed bg-center bg-no-repeat">
         <section className="flex justify-center">
           <h1 className="flex text-5xl md:text-6xl lg:text-7xl font-bold text-black text-center mt-20">
             Design
           </h1>
         </section>
 
-        <section
-          className="flex justify-center"
-          /* className="grid grid-cols-4 s:grid-cols1 md:grid-cols-2 lg:grid-cols-4 gap-x-2 gap-y-6 w-full mt-20 place-self-center justify-items-center" */
-        >
-          {/* <DesignCards /> */}
+        <section className="flex justify-center font-bold m-0">
           <BentoGrid />
         </section>
       </main>
