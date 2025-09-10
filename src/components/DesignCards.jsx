413412413a41d0b593cea4d69e836ffc858acc87
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-09-10 13:02:19 +0200

    Oprettelse af Design komponent og cards JSON

---
+++ src/components/DesignCards.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+export default function DesignCards() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("./data/designCards.json");
+      const json = await response.json();
+      setCards(json);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {cards.map((card, index) => (
+        <a href={card.link} key={index}>
+          <div className="w-80 h-[32rem] bg-black rounded-2xl shadow-md flex flex-col justify-between transition-transform duration-200 hover:scale-105 hover:shadow-xl">
+            <div className="flex flex-col h-1/2 w-full">
+              <h2 className="mt-20 text-center text-white font-bold text-xxl">
+                {card.title}
+              </h2>
+            </div>
+            <div className="h-1/2 w-full flex rounded-b-2xl overflow-hidden">
+              <img
+                className="w-full h-full object-cover"
+                src={card.image}
+                alt={card.alt || card.title}
+              />
+            </div>
+          </div>
+        </a>
+      ))}
+    </>
+  );
+}
--- src/pages/Design.jsx
+++ src/pages/Design.jsx
@@ -1,1 +1,21 @@
-export default function Design() {}
+import Header from "../components/Header";
+
+export default function Design() {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main className="flex justify-center align-bottom min-h-screen w-screen bg-[url('/img/backgrounds/BaggrundDesign.svg')] bg-center bg-no-repeat ">
+        <section className="flex ">
+          <h1 className="flex text-5xl md:text-6xl lg:text-7xl font-bold text-black text-center mt-20">
+            Design
+          </h1>
+        </section>
+
+        <section></section>
+      </main>
+      ;
+    </>
+  );
+}
