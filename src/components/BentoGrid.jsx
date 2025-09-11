e239e341b2ce37f1b9c36936ecf9d8f278ae7ae0
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-09-11 09:25:49 +0200

    Mere space i bentogrid på design

--- src/components/BentoGrid.jsx
+++ src/components/BentoGrid.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 export default function BentoGrid() {
   return (
-    <div className="grid h-screen w-screen grid-cols-4 grid-rows-4 gap-3 xl:m-40 lg:m-32 md:m-16 text-xl">
+    <div className="grid h-screen w-screen grid-cols-4 grid-rows-4 gap-14 xl:m-40 lg:m-32 md:m-16 text-xl">
       <Link
         to="/"
         className="col-span-1 row-span-1 rounded-xl bg-black flex items-center justify-center transition-transform duration-200 hover:scale-105 hover:shadow-xl hover:text-[#ff9100]"
