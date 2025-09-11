5ed84183f0234a393089b7624083c104039a75b7
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-09-11 11:56:12 +0200

    Adjusting Homepage grid gap for responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -68,7 +68,7 @@ function HomePage() {
           </h3>
         </section>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center w-full flex-grow">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center min-w-[100%] xl:min-w-[100%] 2xl:min-w-[60%] place-self-center flex-grow">
           <HomeCards />
         </div>
 
