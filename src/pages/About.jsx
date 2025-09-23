287f10e735e8694aa68932b241ef9dc55b2946d7
multmedia-bjarke <bjarkedtholm@gmail.com>
2025-09-23 17:05:51 +0200

    Update About.jsx
    Tilføjelse af Personlig section indhold og fjernelse af hovereffekter på siden

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
 
       <main className="flex flex-col items-center min-h-screen w-screen bg-[url('/img/backgrounds/BaggrundOm.svg')] bg-cover bg-left bg-blend-color-burn">
         <section className="flex flex-col items-center w-screen">
-          <h1 className=" text-5xl md:text-6xl lg:text-7xl font-bold text-black text-center mt-20 mb-20">
+          <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-black text-center mt-20 mb-20">
             Hvem er jeg?
           </h1>
 
@@ -19,7 +19,7 @@ export default function About() {
             </div>
 
             {/*Tile container*/}
-            <div className="w-[40rem] h-[32rem] bg-black rounded-2xl shadow-md flex flex-col justify-center transition-transform duration-200 hover:scale-105 hover:shadow-xl text-white p-5">
+            <div className="w-[40rem] h-[32rem] bg-black rounded-2xl shadow-md flex flex-col justify-center text-white p-5">
               <div>
                 <h2 className="text-4xl">Kort og godt</h2>
                 <div className="flex flex-col items-center mt-12 text-2xl">
@@ -36,15 +36,16 @@ export default function About() {
           </div>
         </section>
 
+        {/*Section - CV */}
         <section className="flex flex-col items-center w-screen gap-16">
-          <h1 className=" text-5xl md:text-6xl lg:text-7xl font-bold text-black text-center mt-20 mb-20">
-            Karriere
+          <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-black text-center mt-20 mb-20">
+            CV
           </h1>
 
           {/* AK card */}
           <div className="flex w-screen justify-between">
             {/*Tile container*/}
-            <div className="self-start w-[40rem] h-[32rem] bg-black rounded-r-2xl shadow-md flex flex-col justify-center transition-transform duration-200 hover:scale-105 hover:shadow-xl text-white p-5">
+            <div className="self-start w-[40rem] h-[32rem] bg-black rounded-r-2xl shadow-md flex flex-col justify-center text-white p-5">
               <div>
                 <h2 className="text-4xl">AK multimediedesign 2026</h2>
                 <div className="flex flex-col items-center mt-12 text-2xl gap-6 text-center">
@@ -66,7 +67,7 @@ export default function About() {
           {/* BA card */}
           <div className="flex w-screen justify-between flex-row-reverse">
             {/* Card content */}
-            <div className="self-end w-[40rem] h-[32rem] bg-black rounded-l-2xl shadow-md flex flex-col justify-center transition-transform duration-200 hover:scale-105 hover:shadow-xl text-white p-5">
+            <div className="self-end w-[40rem] h-[32rem] bg-black rounded-l-2xl shadow-md flex flex-col justify-center text-white p-5">
               <div>
                 <h2 className="text-4xl">BA of Arts 2024</h2>
                 <div className="flex flex-col items-center mt-12 text-2xl gap-6 text-center">
@@ -86,7 +87,7 @@ export default function About() {
           {/* STX card */}
           <div className="flex w-screen justify-between">
             {/* Card content */}
-            <div className="self-end w-[40rem] h-[32rem] bg-black rounded-r-2xl shadow-md flex flex-col justify-center transition-transform duration-200 hover:scale-105 hover:shadow-xl text-white p-5">
+            <div className="self-end w-[40rem] h-[32rem] bg-black rounded-r-2xl shadow-md flex flex-col justify-center text-white p-5">
               <div>
                 <h2 className="text-4xl">BA of Arts 2024</h2>
                 <div className="flex flex-col items-center mt-12 text-2xl gap-6 text-center">
@@ -106,19 +107,61 @@ export default function About() {
           </div>
         </section>
 
-        <section className="flex justify-center">
+        {/*Section - personligt */}
+        <section className="flex flex-col items-center w-screen gap-16">
           <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-black text-center mt-20 mb-20">
             Personligt
           </h1>
+
+          {/* Card top */}
+          <div className="flex w-screen justify-center">
+            <div className="w-[55rem] h-[20rem] bg-[#ff9100] rounded-2xl shadow-md flex flex-col justify-center text-white p-5 mb-10">
+              <div>
+                <h2 className="text-4xl">Karaktertræk</h2>
+                <div className="flex flex-col items-center mt-12 text-2xl gap-6 text-center">
+                  <p className="w-[60%] text-justify tracking-tight">
+                    “Jeg er en <strong>rolig</strong>,{" "}
+                    <strong>ansvarsbevidst</strong> og{" "}
+                    <strong>initiativrig teamplayer</strong>, der er nede på
+                    jorden, kærlig og hyggelig.”
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Card bottom */}
+          <div className="flex w-screen justify-center">
+            <div className="w-[55rem] h-[20rem] bg-black rounded-2xl shadow-md flex flex-col justify-center text-white p-5">
+              <div>
+                <h2 className="text-4xl">Interesser</h2>
+                <div className="flex flex-col items-center mt-12 text-2xl gap-6 text-center">
+                  <p className="w-[80%] text-justify tracking-tight">
+                    “I min fritid elsker jeg at bruge tid på{" "}
+                    <strong>sangskrivning</strong> og{" "}
+                    <strong>musikproduktion</strong>, samt{" "}
+                    <strong>AV-produktion</strong>. Når jeg ikke gør det{" "}
+                    <strong>løber</strong> jeg fra genbrug til genbrug og
+                    svinger grydeskeen i køkkenet derhjemme.”
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
 
-        <section className="flex justify-center">
+        {/*Section - Hvad kan jeg tilbyde */}
+        <section className="flex flex-col items-center w-screen gap-16">
           <h1 className="flex text-5xl md:text-6xl lg:text-7xl font-bold text-black text-center mt-20">
             Hvad kan jeg tilbyde?
           </h1>
         </section>
 
-        <section className="flex justify-center" id="contact-section">
+        {/*Section - CTA */}
+        <section
+          id="contact-section"
+          className="flex flex-col items-center w-screen gap-16"
+        >
           <h1 className="flex text-5xl md:text-6xl lg:text-7xl font-bold text-black text-center mt-20">
             Giv mig et kald!
           </h1>
